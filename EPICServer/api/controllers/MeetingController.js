adddac5ad5423749e8f5ac1b26e51bdd1ff835eb
Jaco <jaco@peoplesoft.co.za>
2015-06-25 17:42:20 +0200

    added meeting model and added new fields to the user model. Also started on API docs

---
+++ EPICServer/api/controllers/MeetingController.js
@@ -0,0 +1,10 @@
+/**
+ * MeetingController
+ *
+ * @description :: Server-side logic for managing logs
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
--- EPICServer/api/models/Log.js
+++ EPICServer/api/models/Log.js
@@ -1,5 +1,6 @@
 var Log = {
-
+	meeting  : { model: 'meeting', required : true },
+	body : {type: 'string', required: true}
 };
 
 module.exports = Log;
