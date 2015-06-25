f302cf6427cc5e7a4c0f7d88c08d26746292355c
u12003922 <u12003922@tuks.co.za>
2015-06-25 17:43:56 +0200

    app do not restart when new is presented

--- EPICProtectionApp/EPICApp/app/src/main/java/info/projectepic/epicapp/MainActivity.java
+++ EPICProtectionApp/EPICApp/app/src/main/java/info/projectepic/epicapp/MainActivity.java
@@ -1,11 +1,16 @@
 package info.projectepic.epicapp;
 
+import android.app.Activity;
+import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
 import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -14,12 +19,18 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    //private variables used
+    private PendingIntent pendingIntent;
+    private IntentFilter[] intentFiltersArray;
+    private NfcAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +58,54 @@ public class MainActivity extends ActionBarActivity {
         butEnter.setOnClickListener(ListenerEnter);
         Button butLeav = (Button)findViewById(R.id.button2);
         butLeav.setOnClickListener(ListenerLeave);
+        mAdapter = NfcAdapter.getDefaultAdapter(this);
+        pendingIntent = PendingIntent.getActivity(
+                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try {
+            ndef.addDataType("*/*");    /* Handles all MIME based dispatches.
+                                       You should specify only the ones that you need. */
+        }
+        catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("fail", e);
+        }
+        intentFiltersArray = new IntentFilter[] {ndef, };
+        //handleIntent(getIntent());
+    }
+    @Override
+    public void onPause() {
+
+        mAdapter.disableForegroundDispatch(this);
+        super.onPause();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mAdapter.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, null);
+    }
+    @Override
+    public void onNewIntent(Intent intent) {
+        String action = intent.getAction();
+        try
+        {
+            // NFC transfer. Receiving message here.
+            if(action != null && action.equals(NfcAdapter.ACTION_NDEF_DISCOVERED))
+            {
+                Parcelable[] parcelables = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+                NdefMessage inNdefMessage = (NdefMessage) parcelables[0];
+                NdefRecord[] inNdefRecords = inNdefMessage.getRecords();
+                NdefRecord NdefRecord_0 = inNdefRecords[0];
+                String inMsg = new String(NdefRecord_0.getPayload());
+
+                //Toast.makeText(getApplicationContext(), "Toasty: " + inMsg + action.toString(), Toast.LENGTH_LONG).show();
+                TextView tv = (TextView)findViewById(R.id.textView);
+                tv.setText(inMsg);
+            }
+        }
+        catch(Exception e)
+        {
+            //Log.e("NFC", e.getMessage());
+        }
     }
 
     @Override
@@ -72,11 +131,8 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    /*Our coding*/
-    @Override
-    public void onResume() {
-        super.onResume();
-        Intent intent = getIntent();
+    private void handleIntent(Intent intent)
+    {
         NdefMessage msg;
         if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
             Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
@@ -86,9 +142,7 @@ public class MainActivity extends ActionBarActivity {
                 tv.setText(new String(msg.getRecords()[0].getPayload()));
             }
         }
-        //process the msgs array
     }
-
     //Storing info
     List<String> restoreSettings = new ArrayList<String>();
     //My Functions
@@ -148,4 +202,32 @@ public class MainActivity extends ActionBarActivity {
             }
         }
     }
+
+    /*FORGROUND DISPATCHING*/
+    public void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType("text/plain");
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("Check your mime type.");
+        }
+        handleIntent(intent);
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
+    }
+
+    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
+    }
+
 }
