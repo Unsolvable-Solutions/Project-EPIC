db26837c123d6ff158a233a3edbc41461f94504e
Jaco Bezuidenhout <jaco@peoplesoft.co.za>
2015-09-02 19:19:51 +0200

    added new interface and controllers

--- EPICInterface/js/app.js
+++ EPICInterface/js/app.js
@@ -7,59 +7,16 @@ onload = function() {
     nwin.maximize();
 }
 
-var url = "http://localhost:1337";
+var url = "http://projectepic.info";
+var appName = {name: "projectEpic", header: "Project Epic"};
 
-var app = angular.module('paperlessPractice', ['ngRoute'])
+var app = angular.module(appName.name, ['ngRoute'])
 .config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/', {
-        templateUrl: './pages/index.html',
+        templateUrl: './templates/index.html',
         controller: 'indexController'
-      }).
-      when('/splash', {
-        templateUrl: './pages/splash.html',
-        controller: 'splashController'
-      }).
-      when('/patients/:searchText', {
-        templateUrl: './pages/patients.html',
-        controller: 'patientsController'
-      }).
-      when('/patients/', {
-        templateUrl: './pages/patients.html',
-        controller: 'patientsController'
-      }).
-      when('/patient/edit/:patientId', {
-        templateUrl: './pages/editPatient.html',
-        controller: 'editPatientController'
-      }).
-      when('/patient/new', {
-        templateUrl: './pages/newPatient.html',
-        controller: 'newPatientController'
-      }).
-      when('/patient/:patientId', {
-        templateUrl: './pages/patient.html',
-        controller: 'patientController'
-      }).
-      when('/patient', {
-        templateUrl: './pages/patients.html',
-        controller: 'patientsController'
-      }).
-      when('/debtors', {
-        templateUrl: './pages/debtors.html',
-        controller: 'debtorsController'
-      }).
-      when('/debtor/new', {
-        templateUrl: './pages/newDebtor.html',
-        controller: 'newDebtorController'
-      }).
-      when('/debtor/edit/:debtorId', {
-        templateUrl: './pages/editDebtor.html',
-        controller: 'editDebtorController'
-      }).
-      when('/debtors', {
-        templateUrl: './pages/debtors.html',
-        controller: 'debtorsController'
       }).
       otherwise({
         redirectTo: '/'
@@ -102,6 +59,7 @@ app.service('fileUpload', ['$http', function ($http) {
 app.controller('navController', function($scope,$http,$location) 
 {
     var t = this;
+    t.header = appName.header;
 
     t.searchPatient = function()
     {
@@ -116,49 +74,23 @@ app.controller('navController', function($scope,$http,$location)
 
 });
 
-app.controller('splashController', function($scope,$http) 
-{
-    var t = this;
-    
-});
-
 app.controller('indexController', function($scope,$http) 
 {
     var t = this;
     t.test = [1,2,3,4];
 });
 
-app.controller('patientsController', function($scope,$http,$routeParams) 
+app.controller('meetingController', function($scope,$http) 
 {
     var t = this;
-    t.patient = {};
-    t.searchText = $routeParams.searchText;
-
-    t.refresh = function(limit)
+    t.refresh = function()
     {
-        $http.get( url + "/patient?limit=" + (limit || 5000)).
-          then(function(response) {
-            t.patients = response.data;
-          }, function(response) {
-            
-          });
-    }
-    t.refresh();
-});
+        $http.get(url + "/meeting")
+        .then(function(response){
+            t.meetings = response.data;
+        },function(response){
 
-app.controller('debtorsController', function($scope,$http) 
-{
-    var t = this;
-    t.debtor = {};
-
-    t.refresh = function(limit)
-    {
-        $http.get( url + "/debtor?limit=" + (limit || 5000)).
-          then(function(response) {
-            t.debtors = response.data;
-          }, function(response) {
-            
-          });
+        });
     }
     t.refresh();
 });
